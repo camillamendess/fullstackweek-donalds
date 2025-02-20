eb0382ff1de979c1971c57a4be2e81be817f31d5
Camilla <milla_mendes83@hotmail.com>
2025-02-20 20:46:11 -0300

    fix: :bug: fix product-header error

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -1,115 +1,46 @@
 "use client";
 
-import { Prisma } from "@prisma/client";
-import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { Product } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
-import { ScrollArea } from "@/components/ui/scroll-area";
 
-interface ProductDetailsProps {
-  product: Prisma.ProductGetPayload<{
-    include: {
-      restaurant: {
-        select: {
-          name: true;
-          avatarImageUrl: true;
-        };
-      };
-    };
-  }>;
+interface ProductHeaderProps {
+  product: Pick<Product, "name" | "imageUrl">;
 }
 
-const ProductDetails = ({ product }: ProductDetailsProps) => {
-  const [quantity, setQuantity] = useState<number>(1);
-  const handleDecreaseQuantity = () => {
-    setQuantity((prev) => {
-      if (prev === 1) {
-        return 1;
-      }
-      return prev - 1;
-    });
-  };
-  const handleIncreaseQuantity = () => {
-    setQuantity((prev) => prev + 1);
-  };
-
+const ProductHeader = ({ product }: ProductHeaderProps) => {
+  const router = useRouter();
+  const handleBackClick = () => router.back();
   return (
-    <>
-      <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col overflow-hidden rounded-t-3xl p-5">
-        <div className="flex-auto overflow-hidden">
-          {/* RESTAURANTE */}
-          <div className="flex items-center gap-1.5">
-            <Image
-              src={product.restaurant.avatarImageUrl}
-              alt={product.restaurant.name}
-              width={16}
-              height={16}
-              className="rounded-full"
-            />
-            <p className="text-xs text-muted-foreground">
-              {product.restaurant.name}
-            </p>
-          </div>
+    <div className="relative min-h-[300px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
 
-          {/* NOME DO PRODUTO */}
-          <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain"
+      />
 
-          {/* PREÇO E QUANTIDADE */}
-          <div className="mt-3 flex items-center justify-between">
-            <h3 className="text-xl font-semibold">
-              {product.price}
-            </h3>
-            <div className="flex items-center gap-3 text-center">
-              <Button
-                variant="outline"
-                className="h-8 w-8 rounded-xl"
-                onClick={handleDecreaseQuantity}
-              >
-                <ChevronLeftIcon />
-              </Button>
-              <p className="w-4">{quantity}</p>
-              <Button
-                variant="destructive"
-                className="h-8 w-8 rounded-xl"
-                onClick={handleIncreaseQuantity}
-              >
-                <ChevronRightIcon />
-              </Button>
-            </div>
-          </div>
-
-          <ScrollArea className="h-full">
-            {/* SOBRE */}
-            <div className="mt-6 space-y-3">
-              <h4 className="font-semibold">Sobre</h4>
-              <p className="text-sm text-muted-foreground">
-                {product.description}
-              </p>
-            </div>
-
-            {/* INGREDIENTS */}
-            <div className="mt-6 space-y-3">
-              <div className="5 flex items-center gap-1">
-                <ChefHatIcon size={18} />
-                <h4 className="font-semibold">Ingredientes</h4>
-              </div>
-              <ul className="text-muted-fo list-disc px-5 text-sm text-muted-foreground">
-                {product.ingredients.map((ingredient) => (
-                  <li key={ingredient}>{ingredient}</li>
-                ))}
-              </ul>
-            </div>
-          </ScrollArea>
-        </div>
-
-        <Button className="w-full rounded-full" >
-          Adicionar à sacola
-        </Button>
-      </div>
-    </>
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-50 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
   );
 };
 
-export default ProductDetails;
+export default ProductHeader;
