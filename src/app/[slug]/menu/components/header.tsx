ae80aaffeaedfd167d5cdc75ea3385ec91c85c3c
Camilla <milla_mendes83@hotmail.com>
2025-02-23 13:58:40 -0300

    feat: :sparkles: ensure all orders and back buttons work as expected

--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -1,25 +1,23 @@
 "use client";
 
-// Client Component -> é possivel usar hooks e fazer interações, diferente dos server comp.
+import { Restaurant } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
 
-import { Restaurant } from "@prisma/client"
-import { Pick } from "@prisma/client/runtime/library"
-import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react"
-import Image from "next/image"
-import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
-import { Button } from "@/components/ui/button"
-
-
-interface RestauranteHeaderProps {
-  restaurant: Pick<Restaurant, 'name' | 'coverImageUrl'>
+interface RestaurantHeaderProps {
+  restaurant: Pick<Restaurant, "name" | "coverImageUrl">;
 }
 
-const RestaurantHeader = ({ restaurant }: RestauranteHeaderProps) => {
+const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
+  const { slug } = useParams<{ slug: string }>();
   const router = useRouter();
   const handleBackClick = () => router.back();
+  const handleOrdersClick = () => router.push(`/${slug}/orders`);
   return (
-    <div className="relative h-[250px] w-full ">
+    <div className="relative h-[250px] w-full">
       <Button
         variant="secondary"
         size="icon"
@@ -38,11 +36,12 @@ const RestaurantHeader = ({ restaurant }: RestauranteHeaderProps) => {
         variant="secondary"
         size="icon"
         className="absolute right-4 top-4 z-50 rounded-full"
+        onClick={handleOrdersClick}
       >
         <ScrollTextIcon />
       </Button>
     </div>
-  )
-}
+  );
+};
 
 export default RestaurantHeader;
