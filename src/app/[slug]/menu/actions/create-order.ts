0dd6e053486a7147d3d6c4749f462d71d265c640
Camilla <milla_mendes83@hotmail.com>
2025-02-23 12:20:15 -0300

    feat: :sparkles: add orders page

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumtionMethod } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -56,4 +57,6 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
+
+  redirect(`/${input.slug}/orders`);
 };
